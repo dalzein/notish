eac2a0188bb78cd2e19c071593535fa584b2d21b
Daniel <daniel@MacBook-Pro.local>
2023-06-26 02:25:54 +1000

    Update styling

--- src/components/Guide/Guide.js
+++ src/components/Guide/Guide.js
@@ -3,14 +3,14 @@ import "./Guide.css";
 function Guide() {
   return (
     <div className="guide">
-      <h2>Welcome!</h2>
+      <h2>Hello! Just a couple things...</h2>
       <p>
-        Signing in isn't required to use this app - your notes will be saved to
-        your browser's local storage
+        Signing in isn't required to use this app. Your notes will be saved to
+        your browser's local storage.
       </p>
       <p>
         If you decide to sign in, any notes you've created locally will be
-        synced to your account
+        synced to your account.
       </p>
     </div>
   );
